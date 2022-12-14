45783277117442710d8f8ca8ed6b947eb00a9422
Francis <120447693+ranshaka@users.noreply.github.com>
2022-12-14 14:47:11 +0800

    增加更新进度/ 添加使用说明
    增加更新进度/ 添加使用说明

--- README.md
+++ README.md
@@ -1,2 +1,132 @@
 # electron-hot-updata
-electron 热更新  动态跟新electron打包之后文件 
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+- [Contributing](../CONTRIBUTING.md)
+
+## About <a name = "about"></a>
+
+electron的项目用 electron 自动更新，更新之后需要手动安装程序，有需求就有产出;
+
+electron-hot-updata插件，使用本插件热更新，动态替换electron 执行文件达到热更新;
+
+## Getting Started <a name = "getting_started"></a>
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See [deployment](#deployment) for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+What things you need to install the software and how to install them.
+
+```
+npm i electron-hot-updata
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running.
+
+#### 1.  因为electron 默认是安装在C:\Program Files\ 目录下 操作文件需要管理员权限;
+// 在electron-build 的package.json 里面配置权限;
+
+// 我的项目是基于vue  我就直接在vue.config.js 里面配置
+```
+    //因为electron 默认是安装在C:\Program Files\ 目录下 操作文件需要管理员权限;
+     module.exports = {
+        ...,
+         "win": { 
+            "requestedExecutionLevel": "highestAvailable" // highestAvailable 最高权限
+        },
+    }
+
+
+```
+#### 2.   配置项目服务器地址 
+```
+    // vue.config.js
+    1.配置项目服务器地址 
+    module.exports = {
+        ...,
+        publish: [{
+           "provider": "generic",
+           url: "http://192.168.0.191/electron/" // 更新文件服务器地址 放你后台的地址
+         }],
+     }
+        配置之后执行electron-build 打包之后会自动生成 latest.yml 文件
+
+    2.sasr 配置   //看你自己需求  可有可无
+    module.exports = {
+        ...,
+        sasr: false  //  执行文件是否打包成sasr文件
+     }
+
+    sasr:false   // 打包出来的就是 html + css + js   
+    sasr:true   // 打包之后就是sasr 文件
+
+    ##
+    http://192.168.0.191/electron/ 下面存放两个文件
+        latest.yml      
+            系统打包之后自动生成
+        app.zip     
+            //app.zip 分两种情况
+            1.sasr:false 
+                项目/dist_electron/win-unpacked/resources/  下找到app文件夹 打包成 app.zip 文件
+            2.sasr:true 
+                 项目/dist_electron/win-unpacked/resources/  下找到 app.sasr文件 打包成app.zip 文件  
+        
+```
+
+#### 3.  版本号  如果你有更好的修改项目版本号办法  直接无视这一块
+```
+    electron 版本区分默认依靠 项目>package.json>version;
+    所有每次打包之后 都要修改版本号 ；
+    靠版本号来区分是否需要更新
+    
+
+    在vue.cinfog.js 里面添加以下代码  
+    <!-- 一下代码每次运行项目都会修改版本号 -->
+
+    const path = require('path')
+    const fs = require('fs');
+    const resolve=(dir)=> {
+        return path.join(__dirname, dir)
+    }
+    const AddZero = (time)=> {
+        if (time < 10) {
+            return "0" + time
+        } else {
+            return time
+        }
+    }
+    let packageTxt = fs.readFileSync('./package.json', 'utf8');
+    let versionData = packageTxt.split('\n');
+    let packageJson = JSON.parse(packageTxt);
+    let VersionArr = packageJson.version.split('.');
+    let date = new Date();
+    let today = date.getFullYear() + "" + AddZero((date.getMonth() + 1)) + "" + AddZero(date.getDate())
+    if (today == VersionArr[1]) {
+        VersionArr[2] = parseInt(VersionArr[2]) + 1
+    } else {
+        VersionArr[1] = date.getFullYear() + "" + AddZero((date.getMonth() + 1)) + "" + AddZero(date.getDate())
+        VersionArr[2] = 1;
+    }
+    let versionLine = VersionArr.join('.');
+    for (let i = 0; i < versionData.length; i++) {
+        if (versionData[i].indexOf('"version":') != -1) {
+            versionData.splice(i, 1, '  "version": "' + versionLine + '",');
+            break;
+        }
+    }
+    fs.writeFileSync('./package.json', versionData.join('\n'), 'utf8');
+```
+
+
+Say what the step will be
+
+```
+/demo 目录下有案例
+```
+
--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@ const request = require('request')
 const events = require("events");
 const admZip = require('adm-zip')
 const YAML = require('js-yaml');
-const compareVersion = require("./utils.js")
+const utils = require("./utils.js")
 const fs = require("fs")
 const path = require('path');
+
+const progress = require('progress-stream');
+const https = require("https")
+const http = require("http")
 const baseUrl = path.join(__dirname, '../../resources/')
- class electronHotupdata extends events.EventEmitter {
+// const baseUrl = path.join(__dirname,'../dist_electron/bundled/')
+class electronHotupdata extends events.EventEmitter {
     constructor(options) {
         super();
         this.provider = ""
         this.newVersion = ""
         this.usedVersion = ""
-        this.baseUrl=baseUrl
+        this.progressNumber = 0
+        this.baseUrl = baseUrl
+        this.status = "loop"
     }
 
     setBaseUrl(baseUrls) {
@@ -25,11 +32,12 @@ const baseUrl = path.join(__dirname, '../../resources/')
         this.newVersion = newVersion;
     }
     startLoop() {
+        if(this.progressNumber)return
         if (!this.provider || !this.newVersion) {
             this.emit("get-version-failed", "版本号/地址不对")
         }
         try {
-            request(this.provider+"latest.yml",(error, res, body) => {
+            request(this.provider + "latest.yml", (error, res, body) => {
                 if (!res) {
                     return this.emit("get-version-failed", "获取更新版本号失败")
                 }
@@ -37,16 +45,17 @@ const baseUrl = path.join(__dirname, '../../resources/')
                     return this.emit("get-version-failed", "获取更新版本号失败" + res)
                 }
                 let result = YAML.load(body);
-                if(result &&  result.version){
-                    const VersionType=compareVersion(result.version,this.newVersion)
-                    if(VersionType ==  1){
-                         this.emit("new-version-available", "获取到可更新版本 ==== " + result.version )
-                    }else if(VersionType==0){
+                if (result && result.version) {
+                    const VersionType = utils.compareVersion(result.version, this.newVersion)
+                    this.usedVersion = result.version
+                    if (VersionType == 1) {
+                       return this.emit("new-version-available", "获取到可更新版本  ====  " + result.version)
+                    } else if (VersionType == 0) {
                         this.emit("no-new-version", "当前已经是最新版")
-                    }else{
-                        this.emit("no-new-version", "当前已经是最新版")
+                    } else {
+                        this.emit("no-new-version", "获取的老版本")
                     }
-                }else{
+                } else {
                     return this.emit("get-version-failed", "获取更新版本号失败")
                 }
             })
@@ -56,35 +65,71 @@ const baseUrl = path.join(__dirname, '../../resources/')
     }
     startUpdata() {
         if (!this.usedVersion || this.usedVersion == this.newVersion) {
-            this.emit("no-new-version", "当前已经是最新版")
+            this.emit("no-new-version", "当前已经是最新版  ===  "+this.newVersion)
         }
         this.downLoad().then(res => {
+            this.progressNumber=0
+            this.emit("downloaded-progressNumber", 100)
             this.emit("complete-downloaded")
+        }).catch((error)=>{
+            this.progressNumber=0
+            throw error
         })
     }
 
-    downLoad() {
+     async downLoad() {
+        const stream = fs.createWriteStream(`${this.baseUrl}test.zip`);
+        const p = await this.setDownloadProgress(this.provider + "app.zip")
         return new Promise((resolve, reject) => {
-            isFileExisted(this.baseUrl + "app.zip").then(falg => {
-                this.emit("no-new-version", `${this.baseUrl}test.zip`)
-                const stream = fs.createWriteStream(`${this.baseUrl}test.zip`);
-                request(`${this.provider}app.zip?v=${new Date().getTime()}`).pipe(stream).on('close', (error, res, body) => {
-                    console.log("开始解压" + this.provider + 'app.zip')
-                    const unzip = new admZip(`${this.baseUrl}test.zip`);
-                    unzip.extractAllTo(this.baseUrl, /*overwrite*/ true); 
-                    resolve(null)
-                });
-            }).catch(err => {
-                this.emit("downloaded-failed", err)
+            this.emit("no-new-version", `${this.baseUrl}test.zip`)
+            request(`${this.provider}app.zip?v=${new Date().getTime()}`).pipe(p).pipe(stream).on('close', (error, res, body) => {
+                const unzip = new admZip(`${this.baseUrl}test.zip`);
+                unzip.extractAllTo(this.baseUrl, /*overwrite*/ true);
+                resolve(null)
+            });
+        })
+    }
+    // 获取地址
+    async getUrlFileSize(url) {
+        return new Promise(r => {
+            let h = {
+                http,
+                https
+            }
+            let isHttps = url.indexOf("https") != -1
+            h[isHttps ? "https" : "http"].get(url, {
+                rejectUnauthorized: false
+            }, (res) => {
+                r(res.headers['content-length'])
             })
         })
-
     }
-
+    /* 设置下载进度 */
+    async setDownloadProgress(url) {
+        const that=this
+        try {
+            let p = progress({
+                length: await this.getUrlFileSize(url),
+                time: 500
+            })
+            p.on('progress', progress => {
+                let progressNumber=progress.percentage>=99?99:progress.percentage
+                that.progressNumber=progressNumber
+                that.emit("downloaded-progressNumber", that.progressNumber)
+            });
+            return p
+        } catch (error) {
+            that.progressNumber=0
+            that.emit("downloaded-progressNumber", "-1")
+            return error
+             
+        }
+       
+    }
 
 
 }
 
-module.exports ={
+module.exports = {
     electronHotupdata
 }
